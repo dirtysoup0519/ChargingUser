ca5247e87011dc8af42455f81b0603af9e825238
lzyy <hello@lzyy.cn>
2026-09-07 14:33:48 +0800

    fix(ui): declare map ready forwarding signal

--- src/presentation/pages/shell/mainwindow.h
+++ src/presentation/pages/shell/mainwindow.h
@@ -35,6 +35,7 @@ public:
 signals:
     void primaryPageRequested(MainWindow::PrimaryPage page);
     void stationDetailsRequested(const QString &stationId);
+    void mapReady();
     void locateRequested();
     void stationSearchRequested(const QString &keyword);
     void stationSearchRetryRequested();
